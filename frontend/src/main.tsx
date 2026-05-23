f9362418286f4a0b096210d59c38c6ded28e789c
DjaziraOC <djaziranait14@gmil.com>
2026-05-24 00:42:26 +0200

    feat: add main application entry point with routing and state management

---
+++ frontend/src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import './styles/global.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
+
